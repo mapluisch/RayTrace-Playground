ee2d09af4694da90ea741e15b75881c697b60ac9
Martin Pluisch <martin.pluisch@icloud.com>
2020-12-20 01:05:12 +0100

    added movable scene through opencv

--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -12,6 +12,19 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
+int image_width;
+ int image_height;
+ int samples_per_pixel;
+ int max_depth;
+ double vfov;
+ bool randomize_world;
+ Hittable_List world;
+
+ struct RGB {
+    uchar blue;
+    uchar green;
+    uchar red;  };
+
 Color ray_color(const Ray& r, const Hittable& world, int depth) {
     Hit hit;
 
@@ -36,31 +49,31 @@ Hittable_List random_scene() {
     Hittable_List world;
 
     auto ground_material = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
-    world.add(make_shared<Sphere>(Point3(0,-1000,0), 1000, ground_material));
+    world.add(make_shared<Sphere>(Point3(0,-1000.5,0), 1000, ground_material));
 
     for (int a = -5; a < 5; a++) {
         for (int b = -5; b < 5; b++) {
             auto choose_mat = random_double();
-            Point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+            Point3 center(a + 0.9*random_double(), 0, b + 0.9*random_double());
 
-            if ((center - Point3(4, 0.2, 0)).length() > 0.9) {
+            if ((center - Point3(4, 0, 0)).length() > 0.9) {
                 shared_ptr<Material> sphere_material;
 
                 if (choose_mat < 0.6) {
                     // diffuse
                     auto albedo = Color::random() * Color::random();
                     sphere_material = make_shared<Lambertian>(albedo);
-                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                    world.add(make_shared<Sphere>(center, 0, sphere_material));
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = Color::random(0.5, 1);
                     auto fuzz = random_double(0, 0.5);
                     sphere_material = make_shared<Metal>(albedo, fuzz);
-                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                    world.add(make_shared<Sphere>(center, 0, sphere_material));
                 } else {
                     // glass
                     sphere_material = make_shared<Dielectric>(1.5);
-                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                    world.add(make_shared<Sphere>(center, 0, sphere_material));
                 }
             }
         }
@@ -69,6 +82,53 @@ Hittable_List random_scene() {
     return world;
 }
 
+void renderScene(Point3 camPosition, Mat& image){
+    // Camera
+    Point3 camDirection = camPosition + Point3(0,0,-1);
+    Vec3 camUp (0,1,0);
+    double camAperture = 0.0;
+    double camFocusDistance = (camPosition-camDirection).length();
+    Camera cam(camPosition, camDirection, camUp, image_width, image_height, vfov, camAperture, camFocusDistance);
+
+    // Render
+    std::cerr << "-- start rendering --\n";
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\r-- scanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            Color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                Ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world, max_depth);
+            }
+            
+            auto r = pixel_color.x();
+            auto g = pixel_color.y();
+            auto b = pixel_color.z();
+
+                // Divide the color by the number of samples and gamma-correct for gamma=2.0.
+            auto scale = 1.0 / samples_per_pixel;
+            r = sqrt(scale * r);
+            g = sqrt(scale * g);
+            b = sqrt(scale * b);    
+
+            // slow, but working
+            // Vec3b color = image.at<Vec3b>(Point(i,image_height-j));
+            // color[0] = b*255; color[1] = g*255; color[2] = r*255;
+            // image.at<Vec3b>(Point(i,image_height-j)) = color;
+            RGB& rgb = image.ptr<RGB>(image_height - j)[i];
+            rgb.blue = b*255; rgb.green = g*255; rgb.red = r*255;
+
+        }
+    }
+ 
+    std::cerr << "\n-- done rendering, closing output file-stream --\n";
+    // close output-image filestream
+}
+
+
+ 
 
 int main(int argc, char* argv[]) {
     // CXXOPTS Init and Parsing
@@ -83,6 +143,7 @@ int main(int argc, char* argv[]) {
         ("f,fov", "Camera's vertical field of view", cxxopts::value<double>())
         ("o,output", "Output file name", cxxopts::value<std::string>())
         ("r,random", "Boolean whether to create a random scene-world or not", cxxopts::value<bool>())
+        ("e,explore", "Boolean whether to be able to navigate through the scene", cxxopts::value<bool>())
     ;
     auto result = options.parse(argc, argv);
 
@@ -93,24 +154,25 @@ int main(int argc, char* argv[]) {
 
     // CXXOPTS Parsing
     // Read and store passed in arguments or default to 16:9-resolution with 50 samples per pixel
-    const int image_width       = (result.count("width") ? result["width"].as<int>() : 400);
-    const int image_height      = (result.count("height") ? result["height"].as<int>() : 225);
-    const int samples_per_pixel = (result.count("samples_per_pixel") ? result["samples_per_pixel"].as<int>() : 50);
-    const int max_depth         = (result.count("depth") ? result["depth"].as<int>() : 50);
-    const double vfov           = (result.count("fov") ? result["fov"].as<double>() : 90);
-    const bool randomize_world  = (result.count("random") ? result["random"].as<bool>() : false);
+    image_width         = (result.count("width") ? result["width"].as<int>() : 400);
+    image_height        = (result.count("height") ? result["height"].as<int>() : 225);
+    samples_per_pixel   = (result.count("samples_per_pixel") ? result["samples_per_pixel"].as<int>() : 50);
+    max_depth           = (result.count("depth") ? result["depth"].as<int>() : 50);
+    vfov                = (result.count("fov") ? result["fov"].as<double>() : 90);
+    randomize_world     = (result.count("random") ? result["random"].as<bool>() : false);
+    bool explore        = (result.count("explore") ? result["explore"].as<bool>() : false);
     //
 
-    // Open and overwrite image file
-    std::ofstream output_image;
-    std::string output_filename = (result.count("output") ? result["output"].as<std::string>() : "output.ppm");
-    output_image.open(output_filename);
+    // // Open and overwrite image file
+    // std::ofstream output_image;
+    // std::string output_filename = (result.count("output") ? result["output"].as<std::string>() : "output.ppm");
+    // output_image.open(output_filename);
 
     
     
 
     // World
-    Hittable_List world;
+    
     if(randomize_world){
         world = random_scene();
     } else {
@@ -126,41 +188,51 @@ int main(int argc, char* argv[]) {
         world.add(make_shared<Sphere>(Point3( 1.0,    0.0, -1.0),   0.5, material_right)); 
     }
 
-
-    // Camera
-    Point3 camPosition (-1,1,1);
-    Point3 camDirection (0,0,-1);
-    Vec3 camUp (0,1,0);
-    double camAperture = 0.0;
-    double camFocusDistance = (camPosition-camDirection).length();
-    Camera cam(camPosition, camDirection, camUp, image_width, image_height, vfov, camAperture, camFocusDistance);
-
-    // Render
-    std::cerr << "-- start rendering --\n";
-    output_image << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-    for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\r-- scanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            Color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
-                Ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
-            }
-            write_color(output_image, pixel_color, samples_per_pixel);
-        }
-    }
- 
-    std::cerr << "\n-- done rendering, closing output file-stream --\n";
-    // close output-image filestream
-    output_image.close();
+    Mat image (image_height, image_width, CV_8UC3, Scalar(0,0,0));
+    Point3 currentCamPosition (0,0,0);
 
     #pragma region OpenCV testing ------------------------------
+    if(explore){
+        float explore_step_size = 0.25;        
+        while(explore){
+            // set low-quality setting for faster rendering
+            samples_per_pixel = 1;
+            max_depth = 2;
+            image_width = 400;
+            image_height = 225;
+            
 
-    cv::Mat input = cv::imread("output.ppm", IMREAD_COLOR);
-    namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
-     imshow("Display window", input);
-    cv::waitKey(0);
+            
+            namedWindow("RayTracer", WINDOW_AUTOSIZE);// Create a window for display.
+            imshow("RayTracer", image);
+            
+            char key = cv::waitKey(0);
+
+            if (key == 2){ // left arrow
+                currentCamPosition = currentCamPosition + Point3(-explore_step_size,0,0);
+            } else if (key == 0 || key == 87 || key == 119){ // up arrow or uppper/lower-case w
+                currentCamPosition = currentCamPosition + Point3(0,0,-explore_step_size);
+            } else if (key == 3){ // right arrow
+                currentCamPosition = currentCamPosition + Point3(explore_step_size,0,0);
+            } else if (key == 1){ // dowm arrow
+                currentCamPosition = currentCamPosition + Point3(0,0,explore_step_size);
+            } else if (key == 32){ // spacebar
+                // fully render
+                samples_per_pixel = 25;
+                max_depth = 10;
+                image_width = 960;
+                image_height = 540;
+                resize(image, image, cv::Size(image_width, image_height));
+                resizeWindow("RayTracer", image_width, image_height);
+            } else if (key == 27){ // escape, end loop
+                explore = false;
+            }
+            renderScene(currentCamPosition, image);
+        }   
+    } else {
+        namedWindow("RayTracer", WINDOW_AUTOSIZE);// Create a window for display.
+        imshow("RayTracer", image);    
+        cv::waitKey(0);
+    }
     #pragma endregion
 }
